888c33791051f504fdf088ddc88c7ae09e930798
fedgut <fedgut@gmail.com>
2019-11-06 15:16:19 -0600

    Add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,21 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-  def new
+  def new; end
 
-  end
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password_digest)
   end
 end
